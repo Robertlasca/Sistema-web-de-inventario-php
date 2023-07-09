644d7f94109aa65af78f553086000c74091c3a53
Robertlasca <roberto.lasca123@gmail.com>
2023-07-09 15:40:09 -0600

    feat:Revenue and detail_revenue model was done

---
+++ siste/app/Models/Ingreso.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ingreso extends Model
+{
+    protected $table='ingreso';
+    protected $primaryKey='idingreso';
+    public $timestamps=false;
+
+    protected $fillable=[
+        'idprovedor',
+        'tipo_comprobante',
+        'serie_comprobante',
+        'num_comprobante',
+        'fecha_hora',
+        'impuesto',
+        'estado'
+    ];
+
+    protected $guarded=[];
+}
