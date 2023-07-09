b9fd8a1335807fc7fb95f3cebeeaa71d03e8b7f1
Robertlasca <roberto.lasca123@gmail.com>
2023-07-09 14:24:24 -0600

    feat:the client list template was created

---
+++ siste/resources/views/almacen/ventas/cliente/modal.blade.php
@@ -0,0 +1,40 @@
+<div class="modal fade modal-slide-in-right" aria-hidden="true" role="dialog" tabindex="-1" id="modal-delete-{{$per->idpersona}}">
+	<form action="{{ action('PersonaController@destroy', $per->idpersona) }}" method="POST">
+    @method('DELETE')
+    @csrf
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">
+
+						<span aria-hidden="true">
+							X
+						</span>
+						
+					</button>
+
+					<h4 class="modal-title">
+						Eliminar cliente
+					</h4>
+					
+				</div>
+
+				<div class="modal-body">
+					<p> Confirme si deses eliminar el cliente</p>
+				</div>
+
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal-body">
+						Cerrar
+					</button>
+
+					<button type="submit" class="btn btn-primary">
+						Confirmar
+					</button>
+				</div>
+			</div>
+		</div>
+		
+	</form>
+	
+</div>
